c2e299a59794e20be935544248be89facfcbb779
Sheilh-22 <konnehsidikim521@gmail.com>
2022-10-25 15:13:40 -0700

    My 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
 /**
  * print_array - prints n elements of an array
  * @a: array
- * @n: number of value to be printed
+ * @n: number of values to be printed
  */
 void print_array(int *a, int n)
 {
